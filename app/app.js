f1d23be3c8f27f93d79f859567c20b77967c2487
Jack Franklin <jack@jackfranklin.net>
2014-10-27 21:52:40 +0000

    commit 1

---
+++ app/app.js
@@ -0,0 +1,7 @@
+import red from './red';
+import $ from '../lib/jquery';
+
+$(function() {
+  red();
+});
+red();
---
+++ app/red.js
@@ -0,0 +1,5 @@
+import $ from '../lib/jquery'
+
+export default function() {
+  $('p').css('color', 'red');
+};
